63343098b89660f4f5a9045fac7e3f5c74a56c71
kthss01 <kthss01@naver.com>
2021-09-13 18:53:35 +0900

    템플릿 상속, 변수 사용하기

--- codeit-django/costaurant/foods/views.py
+++ codeit-django/costaurant/foods/views.py
@@ -1,7 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, 'foods/index.html')
+    # 날짜 데이터 생성하기
+    today = datetime.today().date()
+    # print(today) # 개발 서버 콘솔에 출력
+    context = {"date" : today}
+    return render(request, 'foods/index.html', context=context)
--- django-pratice/costaurant/menus/views.py
+++ django-pratice/costaurant/menus/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse('<h1>코스토랑 오픈!</h1>')
+    today = datetime.now().date()
+    context = {"today" : today}
+    return render(request, 'menus/index.html', context)
