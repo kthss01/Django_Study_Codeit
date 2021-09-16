6fcd0311d22def0d0cdfcdfa69e7c299ed91970c
kthss01 <kthss01@naver.com>
2021-09-16 18:54:14 +0900

    마이그레이션, 모델 CRUD

--- codeit-django/costaurant/foods/models.py
+++ codeit-django/costaurant/foods/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField() # price
+    img_path = models.CharField(max_length=255)
+    
+    # 하나의 객체를 문자열로 표현
+    def __str__(self):
+        return self.name
--- django-pratice/costaurant/menus/models.py
+++ django-pratice/costaurant/menus/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=80) # 음식이름(한글)
+    name_eng = models.CharField(max_length=80) # 음식이름(영어)
+    description = models.CharField(max_length=120) # 음식설명
+    price = models.IntegerField() # 음식가격
+    img_path = models.CharField(max_length=255) # 사진 파일 경로
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
