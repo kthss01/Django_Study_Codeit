dda4c63a546d79a663b634546cf1e4d7f499c3f0
kthss01 <kthss01@naver.com>
2021-09-17 03:10:51 +0900

    관리자 도구 사용하기, model 적용하기

--- codeit-django/costaurant/foods/views.py
+++ codeit-django/costaurant/foods/views.py
@@ -3,13 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.http import Http404
+from foods.models import Menu
 
 # Create your views here.
 def index(request):
+    context = dict()
     # 날짜 데이터 생성하기
     today = datetime.today().date()
     # print(today) # 개발 서버 콘솔에 출력
-    context = {"date" : today}
+    context["date"] = today
+    
+    menus = Menu.objects.all()
+    context['menus'] = menus
+    
     return render(request, 'foods/index.html', context=context)
 
 def food_detail(request, food):
