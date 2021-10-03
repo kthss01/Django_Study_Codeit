bf059537ee4de36ff14790121a9884d608a058a5
kthss01 <kthss01@naver.com>
2021-10-04 02:02:13 +0900

    폼과 디자인 적용하기

--- django-allauth/coplate_project/coplate_project/urls.py
+++ django-allauth/coplate_project/coplate_project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', include("coplate.urls")),
     # allauth
     path("email-confirmation-done",
-        TemplateView.as_view(template_name="coplate/email_confirmation_done.html"),
+        TemplateView.as_view(template_name="account/email_confirmation_done.html"),
         name="account_email_confirmation_done"
     ),
     path('password/change/', 
--- django-allauth/podomarket_project/podomarket_project/urls.py
+++ django-allauth/podomarket_project/podomarket_project/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', include('podomarket.urls')),
     # allauth
     path("email-confirmation-done",
-        TemplateView.as_view(template_name="podomarket/email_confirmation_done.html"),
+        TemplateView.as_view(template_name="account/email_confirmation_done.html"),
         name="account_email_confirmation_done"
     ),
     path("password/change/",
