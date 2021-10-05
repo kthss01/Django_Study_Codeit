2d649b0f9277c6a8ecb700ddef33f5eb0982a112
kthss01 <kthss01@naver.com>
2021-10-06 00:00:37 +0900

    django 배포 하기

--- django-allauth/coplate_project/coplate_project/settings.py
+++ django-allauth/coplate_project/coplate_project/settings.py
@@ -157,4 +157,9 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 
 # Email settings
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "kthtim0704roba@gmail.com"
+EMAIL_HOST_PASSWORD = "alrvfuvexagadxre"
